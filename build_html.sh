1f2f2cc5c44381d337b1617afdb61095f04c344f
hanjr92 <jiaronghan@outlook.com>
2023-04-19 17:42:29 +0800

    update

--- build_html.sh
+++ build_html.sh
@@ -6,5 +6,5 @@ rm -rf _build/rst _build/html
 d2lbook build rst
 cp static/frontpage.html _build/rst/
 d2lbook build html
-cp -r static/image/* _build/html/_images/
+cp -r static/image _build/html/_images
 python3 tools/format_tables.py
